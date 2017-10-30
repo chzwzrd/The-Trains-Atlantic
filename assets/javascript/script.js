93b707d4cb663e1589030d434124d4101ccfe0d2
Melodie Chi <Melodie@Apple.local>
2017-10-30 16:10:08 -0700

    initial html/bootstrap setup

---
+++ assets/javascript/script.js
@@ -0,0 +1,25 @@
+// Initialize Firebase
+// ====================================================================================
+var config = {
+	apiKey: "AIzaSyDnVwcgFzSZpuLFwJpC_076O7jkuvu5Zzo",
+	authDomain: "wk-7-hw-train-scheduler.firebaseapp.com",
+	databaseURL: "https://wk-7-hw-train-scheduler.firebaseio.com",
+	projectId: "wk-7-hw-train-scheduler",
+	storageBucket: "",
+	messagingSenderId: "87589256783"
+};
+firebase.initializeApp(config);
+
+
+// GLOBAL VARIABLES
+// ====================================================================================
+
+
+
+// FUNCTIONS
+// ====================================================================================
+
+
+
+// MAIN PROCESS
+// ====================================================================================
