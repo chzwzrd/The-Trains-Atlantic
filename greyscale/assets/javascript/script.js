4ec10db8d0dea8fb134764c85ed3a4f487ee6ce8
Melodie Chi <Melodie@Apple.local>
2017-11-19 19:48:20 -0800

    added .trim() when capturing DOM values

--- greyscale/assets/javascript/script.js
+++ greyscale/assets/javascript/script.js
@@ -34,10 +34,10 @@ function civilianTime(time) {
 function calcMinAway() {
 
 	// 1) grab first time train arrives
-	firstTime = $("#time-input").val();
+	firstTime = $("#time-input").val().trim();
 
 	// 2) grab frequency of train
-	frequency = $("#frequency-input").val();
+	frequency = $("#frequency-input").val().trim();
 
 	// 3) grab current time
 	var currentTime = moment();
@@ -128,10 +128,10 @@ function displayNewRow(array) {
 function renderInput() {
 	
 	// capture all of the input values
-	trainName = $("#name-input").val();
-	destination = $("#destination-input").val();
-	firstTime = $("#time-input").val();
-	frequency = $("#frequency").val();
+	trainName = $("#name-input").val().trim();
+	destination = $("#destination-input").val().trim();
+	firstTime = $("#time-input").val().trim();
+	frequency = $("#frequency").val().trim();
 	minutesAway = calcMinAway();
 	nextArrival = calcNextArrival();
 
